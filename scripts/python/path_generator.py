1deafa2c261dd89070dd2222294a86e3561e207c
yulunz <yulunz@umich.edu>
2022-11-06 09:59:24 -0500

    add part one path

--- scripts/python/path_generator.py
+++ scripts/python/path_generator.py
@@ -30,13 +30,32 @@ path = np.array(
         [0, 0, 0],
     ]
 )
+path = np.array([
+    [0, 0, 0],
+    [0.61, 0, -np.pi/2],
+    [0.61, -0.61, 0],
+    [0.61*2, -0.61, np.pi/2],
+    [0.61*2, -0.61+1.22, 0],
+    [0.61*3, -0.61+1.22, -np.pi/2],
+    [0.61*3, -0.61, 0],
+    [0.61*4, -0.61, np.pi/2],
+    [0.61*4, 0, 0],
+    [0.61*5, 0, 0]
+])
+
+# path = np.array(
+#     [[0, 0, 0], [1, 0, np.pi / 2], [1, 1, np.pi], [0, 1, -np.pi / 2], [0, 0, 0]]
+# )
+# path = np.array(
+#     [[0, 0, 0], [1, 0, np.pi / 2], [0, 0, 0]]
+# )
 #path = np.flip(path, 0)
 path_cmd = robot_path_t()
 path_cmd.path_length = len(path)
 for (x, y, theta) in path:
     pose = pose_xyt_t()
-    pose.x = x * 3
-    pose.y = y * 3
+    pose.x = x
+    pose.y = y
     pose.theta = theta
     path_cmd.path.append(pose)
 
