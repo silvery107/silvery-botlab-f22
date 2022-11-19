b4584bbd386cff80c8fd63b35f12b3485857d9fb
silvery107 <1079931505@qq.com>
2022-11-18 21:04:02 -0500

    increase ray stride and std dev

--- mbot/mbot_autonomy/src/slam/sensor_model.cpp
+++ mbot/mbot_autonomy/src/slam/sensor_model.cpp
@@ -6,7 +6,7 @@
 #include <common_utils/geometric/point.hpp>
 #include <slam/mapping.hpp>
 SensorModel::SensorModel(void)
-:   ray_stride_(1)
+:   ray_stride_(5)
 {
 }
 
@@ -17,46 +17,48 @@ double SensorModel::likelihood(const mbot_lcm_msgs::particle_t& sample,
     double likelihood = 0.0;
     MovingLaserScan movingScan(scan, sample.parent_pose, sample.pose, ray_stride_);
     
-    for(const auto& ray : movingScan)
-    {
-        likelihood += scoreRaySimple(ray, map);
-    }
+    // for(auto&& ray : movingScan)
+    // {
+    //     likelihood += scoreRaySimple(ray, map);
+    // }
 	
-	// likelihood = scoreScanBeamModel(movingScan, map);
+	likelihood = scoreScanBeamModel(movingScan, map);
 
 	return likelihood;
 }
 
 double SensorModel::scoreScanBeamModel(const MovingLaserScan& movingScan, const OccupancyGrid& map){
     float maxRange = 5.0f;
-    float stdDev = 1.0f;
+    float stdDev = 4.f;
 
     float lnConstCoeff = log(1 / (stdDev * sqrt(2 * M_PI)));
     float rangeSim;
     float rangeDiff;
-    float sumLogLikelihoods = 0.0;
+    float sumLogLikelihoods = 0.0f;
     int numLogLikelihoods = 0;
     int numInvalids = 0;
-    for (int i = 0; i < movingScan.size(); i += ray_stride_) {
-    	if (movingScan[i].range > maxRange) {
+	for (auto&& ray : movingScan){
+    	if (ray.range > maxRange) {
     		numInvalids += 1;
     		continue;
     	}
     	rangeSim = simulate_ray(
-    			movingScan[i].origin,
-    			movingScan[i].theta,
-    			maxRange, map);
+    			ray.origin,
+    			ray.theta,
+    			maxRange, 
+				map);
     	if (rangeSim < 0) {
     		numInvalids += 1;
     		continue;
     	}
-    	rangeDiff = movingScan[i].range - rangeSim;
-    	sumLogLikelihoods += lnConstCoeff - 0.5 * pow(rangeDiff / stdDev, 2);
+    	rangeDiff = ray.range - rangeSim;
+    	sumLogLikelihoods += lnConstCoeff - 0.5 * pow(rangeDiff / stdDev, 2.0);
     	numLogLikelihoods += 1;
-    }
+	}
+
     sumLogLikelihoods += numInvalids * sumLogLikelihoods / numLogLikelihoods;
-    double likelihood = exp(sumLogLikelihoods);
-    return likelihood;
+    
+	return exp(sumLogLikelihoods);
 }
 
 float SensorModel::simulate_ray(
@@ -78,7 +80,7 @@ float SensorModel::simulate_ray(
 	Point<int> originCell = global_position_to_grid_cell(origin, map);
 	int minRangeCellsSquared = -1;
 	int rangeCellsSquared;
-	for (auto& cell : rayCells) {
+	for (auto&& cell : rayCells) {
 		if (map.isCellInGrid(cell.x, cell.y)) {
 			if (map.logOdds(cell.x, cell.y) > 0) {
 				rangeCellsSquared = pow(originCell.x - cell.x, 2) + pow(originCell.y - cell.y, 2);
