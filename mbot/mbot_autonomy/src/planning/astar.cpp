519a1b54590bb18174518458116565d7e6111313
silvery107 <1079931505@qq.com>
2022-11-29 13:54:43 -0500

    update partial astar

--- mbot/mbot_autonomy/src/planning/astar.cpp
+++ mbot/mbot_autonomy/src/planning/astar.cpp
@@ -1,3 +1,4 @@
+#include <common_utils/timestamp.h>
 #include <planning/astar.hpp>
 #include <algorithm>
 #include <chrono>
@@ -12,6 +13,22 @@ mbot_lcm_msgs::robot_path_t search_for_path(mbot_lcm_msgs::pose_xyt_t start,
     cell_t goalCell = global_position_to_grid_cell(Point<double>(goal.x, goal.y), distances);
      ////////////////// TODO: Implement your A* search here //////////////////////////
     mbot_lcm_msgs::robot_path_t path;
+
+    bool path_found = false;
+    Point<int> start_cell = global_position_to_grid_cell(Point<double>(start.x, start.y), distances);
+    Point<int> goal_cell = global_position_to_grid_cell(Point<double>(goal.x, goal.y), distances);
+    Node start_node(start_cell.x, start_cell.y);
+    Node goal_node(goal_cell.x, goal_cell.y);
+
+    // printf("Get start node (%d, %d), goal node (%d, %d)\n", start_cell.x, start_cell.y, goal_cell.x, goal_cell.y);
+
+    if (start_node==goal_node){
+        path.path.push_back(start);
+        path.path_length = path.path.size();
+        path.utime = utime_now();
+        return path;
+    }
+
     return path;
 }
 
@@ -21,12 +38,23 @@ double h_cost(Node* from, Node* goal, const ObstacleDistanceGrid& distances)
 {
     // TODO: Return calculated h cost
     double h_cost = 0;
+    int dx = abs(from->cell.x - goal->cell.x);
+    int dy = abs(from->cell.y - goal->cell.y);
+    
+    h_cost = (dx + dy) + (1.414 - 2) * std::min(dx, dy);
     return h_cost;
 }
 double g_cost(Node* from, Node* goal, const ObstacleDistanceGrid& distances, const SearchParams& params)
 {
     // TODO: Return calculated g cost
-    double g_cost = 0;   
+    double g_cost = 0;
+    int dx = abs(from->cell.x - goal->cell.x);
+    int dy = abs(from->cell.y - goal->cell.y);
+    if (dx + dy > 1){
+        g_cost = 1.414;
+    } else{
+        g_cost = 1;
+    }
     return g_cost;
 }
 
@@ -34,6 +62,22 @@ std::vector<Node*> expand_node(Node* node, const ObstacleDistanceGrid& distances
 {
     // TODO: Return children of a given node that are not obstacles
     std::vector<Node*> children;
+    // Expand to 8 ways
+    const int x_deltas[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+    const int y_deltas[8] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+    for (int n = 0; n < 8; ++n)
+    {
+        Node *adjacent_node = new Node(node->cell.x + x_deltas[n], node->cell.y + y_deltas[n]);
+        if (distances.isCellInGrid(adjacent_node->cell.x, adjacent_node->cell.y))
+        {
+            // children.push_back(adjacent_node);
+            float cell_distance = distances(adjacent_node->cell.x, adjacent_node->cell.y);
+            if (cell_distance>params.minDistanceToObstacle){//} && cell_distance<params.maxDistanceWithCost){
+                children.push_back(adjacent_node);
+            }
+        }
+    }
     return children;
 }
 
@@ -41,6 +85,12 @@ std::vector<Node*> extract_node_path(Node* goal_node, Node* start_node)
 {
     // TODO: Generate path by following parent nodes
     std::vector<Node*> path;
+    Node *cur_node = goal_node;
+    while (cur_node->parent != start_node){
+        path.push_back(cur_node);
+        cur_node = cur_node->parent;
+    }
+    path.push_back(start_node);
     return path;
 }
 
@@ -49,6 +99,13 @@ std::vector<mbot_lcm_msgs::pose_xyt_t> extract_pose_path(std::vector<Node*> node
 {
     // TODO: prune the path to generate sparse waypoints
     std::vector<mbot_lcm_msgs::pose_xyt_t> path;
+    for (int i=nodes.size()-1; i>=0; i--){
+        mbot_lcm_msgs::pose_xyt_t pose = {0};
+        Point<double> global_pose = grid_position_to_global_position(nodes[i]->cell, distances);
+        pose.x = global_pose.x;
+        pose.y = global_pose.y;
+        path.push_back(pose);
+    }
     return path;
 }
 
